91153a15428975b8c3bc2bc100f15ed1a1223d1d
Lucas <ltavares@fingerprint.com.br>
2020-10-14 14:24:24 -0300

    URL da api e loop infinito arrumado

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tasks/', include('send_email.urls'))
+    path('sendemail/', include('send_email.urls'))
 ]
